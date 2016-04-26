814e9407f0bdd46cc97ee4200b76248e90f5f3ab
张顶炜 <zhangdingwei@souche.com>
2016-04-26 18:53:10 +0800

    make travis happy

--- config/config.exs
+++ config/config.exs
@@ -27,4 +27,7 @@ use Mix.Config
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
-import_config "#{Mix.env}.exs"
+# import_config "#{Mix.env}.exs"
+if Mix.env in [:test, :docs] do
+  config :alchemic_avatar, app_name: :alchemic_avatar
+end
--- config/test.exs
+++
@@ -1,3 +0,0 @@
-use Mix.Config
-
-config :alchemic_avatar, app_name: :alchemic_avatar
